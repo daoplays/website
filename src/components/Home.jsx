42b4961de94144b22aa8c7a93cdd5b62df3c2a5e
Lindley Lentati <daoplays@outlook.com>
2022-06-29 13:29:19 +0100

    start adding better mobile behaviour

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,45 +1,82 @@
 import React from "react";
 import {Button, Card, Col, Container} from 'react-bootstrap';
 import { Link } from "react-router-dom";
+import { isMobile } from "react-device-detect";
 
+const higherOrderComponent = WrappedComponent => {
+    class HOC extends React.Component {
+      render() {
+        return <WrappedComponent />
+      }
+    }
+    return HOC
+  }
 
-function RowCard() 
+function NoDiv() 
 {
     return (
-    <Card style={{flexDirection: "row"}}>
-      <Card.Img style={{width: "25%",objectFit: "cover"}} src="logo_no_text.png" alt="banner" />
-      <Card.Body>
-        <div>
-        <Card.Title className="h3 text-center mb-2 pt-2 font-weight-bold text-secondary"
-        style={{ fontSize: "3rem" }}>
-          DaoPlays is Live!
-        </Card.Title>
+        <></>
+    );
+}
 
-        <Card.Subtitle
-          className="text-center text-secondary mb-3 font-weight-light text-uppercase"
-          style={{ fontSize: "0.8rem" }}
-        >
-          June 28 2022
-        </Card.Subtitle>
+function BlogButton()
+{
+    return (
+        <Button variant="outline-dark" className="font-weight-bold">
+        "View Blog"
+        </Button>
+    );
+}
 
-        <Card.Text
-          className="text-secondary mb-4"
-          style={{ fontSize: "1rem" }}
-        >
-        <br/>
-        At DaoPlays we are planning on using blockchain technology to build apps that bring people together and raise money for good causes. At the moment there isn't much to see, but for now you can check out our blog as we continue to bring our first Solana app into it's public beta on devnet in just a few weeks!  
-        </Card.Text>
-        </div>
-        <div className="text-center">
-        <Link to="/blog">
-            <Button variant="outline-dark" className="font-weight-bold">
-            View Blog
-            </Button>
-        </Link>
-        </div>
-      </Card.Body>
-    </Card>
-);
+const blog_post = {
+    title:"DaoPlays is Live!",
+    sub_title:"June 28 2022",
+    post_text:"At DaoPlays we are planning on using blockchain technology to build apps that bring people together and raise money for good causes. At the moment there isn't much to see, but for now you can check out our blog as we continue to bring our first Solana app into it's public beta on devnet in just a few weeks!",
+    image:"logo_no_text.png",
+    second_div: BlogButton,
+    display_image: !isMobile
+
+};
+
+function RowCard({title, sub_title, post_text, image, second_div, display_image}) 
+{
+    const Second_Div_Component = higherOrderComponent(second_div);
+
+    return (
+        <Card style={{flexDirection: "row"}}>
+
+            
+            {display_image &&  <Card.Img className=".d-none" style={{width: "25%",objectFit: "cover"}} src={image} alt="banner" />}
+            <Card.Body>
+                <div>
+                    <Card.Title 
+                        className="h3 text-center mb-2 pt-2 font-weight-bold text-secondary"
+                        style={{ fontSize: "3rem" }}    
+                    >
+                        {title}
+                    </Card.Title>
+
+                    <Card.Subtitle
+                        className="text-center text-secondary mb-3 font-weight-light text-uppercase"
+                        style={{ fontSize: "0.8rem" }}
+                    >
+                        {sub_title}
+                    </Card.Subtitle>
+
+                    <Card.Text
+                    className="text-secondary mb-4"
+                    style={{ fontSize: "1rem" }}
+                    >
+                        <br/>
+                        {post_text}
+                    </Card.Text>
+                </div>
+                <div className="text-center">
+                    <Second_Div_Component/>
+                </div>
+            </Card.Body>
+        </Card>
+    );
 }
 
 
@@ -50,7 +87,9 @@ function Home() {
           <Container  >
 
               <Col>
-                <RowCard />
+              <Link to="/blog">
+                    <RowCard {...blog_post}/>
+                </Link>
               </Col>
     
           </Container>
